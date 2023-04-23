5963b391b4915787ba839d39f2064f9ad9d52365
YitavGil <yitavga@yahoo.com>
2023-04-23 20:05:59 +0300

    project setup

---
+++ frontend/src/App.jsx
@@ -0,0 +1,12 @@
+
+function App() {
+
+  return (
+<div>
+  <h1 className='text-indigo-400 text-lg'>demn boiii</h1>
+  <p className="text-3xl font-bold underline">shit </p>
+</div>
+  )
+}
+
+export default App

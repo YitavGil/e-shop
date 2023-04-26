8d751be8853563880aebaa621679ced9744c05e3
YitavGil <yitavga@yahoo.com>
2023-04-26 20:15:00 +0300

    bakend setup

--- backend/app.js
+++ backend/app.js
@@ -1,6 +1,16 @@
-const express = require('express');
+const express = require("express");
+const ErrorHandler = require("./middleware/error");
+const cookieParser = require("cookie-parser");
+const bodyParser = require("body-parser");
+const cors = require("cors");
+const fileUpload = require("express-fileupload");
 
 const app = express()
+
+app.use(express.json());
+app.use(cookieParser());
+app.use(bodyParser.urlencoded({ extended: true, limit: "50mb" }));
+app.use(fileUpload({useTempFiles: true}));
 
 //config 
 if(process.env.NODE_ENV !== 'PRODUCTION'){

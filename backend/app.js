f47400c4887dadb5f5e53a658cc662c5b8a4dcdf
YitavGil <yitavga@yahoo.com>
2023-04-29 09:22:05 +0300

    multer setup

--- backend/app.js
+++ backend/app.js
@@ -3,14 +3,12 @@ const ErrorHandler = require("./middleware/error");
 const cookieParser = require("cookie-parser");
 const bodyParser = require("body-parser");
 const cors = require("cors");
-const fileUpload = require("express-fileupload");
 
 const app = express()
 
 app.use(express.json());
 app.use(cookieParser());
 app.use(bodyParser.urlencoded({ extended: true, limit: "50mb" }));
-app.use(fileUpload({useTempFiles: true}));
 
 //config 
 if(process.env.NODE_ENV !== 'PRODUCTION'){

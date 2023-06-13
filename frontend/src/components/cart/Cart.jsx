52eb2878fbfd4f33be7e746b6d01639d4735b8c6
YitavGil <yitavga@yahoo.com>
2023-06-13 16:51:34 +0300

    Cart complete

--- frontend/src/components/cart/Cart.jsx
+++ frontend/src/components/cart/Cart.jsx
@@ -1,4 +1,4 @@
-import { useState} from "react";
+import { useState } from "react";
 import PropTypes from "prop-types";
 import styles from "../../styles/styles";
 
@@ -47,15 +47,22 @@ const Cart = ({ setOpenCart }) => {
           {/* cart single items */}
           <br />
           <div className="w-full border-t">
-          {cartData &&
-                  cartData.map((i, index) => (
-                    <CartSingle
-                      key={index}
-                      data={i}
-                      
-                    />
-                  ))}
+            {cartData &&
+              cartData.map((i, index) => <CartSingle key={index} data={i} />)}
           </div>
+        </div>
+
+        <div className="px-5 mb-3">
+              {/* checkout buttons */}
+              <Link to="/checkout">
+                <div
+                  className={`h-[45px] flex items-center justify-center w-[100%] bg-[#e44343] rounded-[5px]`}
+                >
+                  <h1 className="text-[#fff] text-[18px] font-[600]">
+                    Checkout Now (USD${totalPrice})
+                  </h1>
+                </div>
+              </Link>
         </div>
       </div>
     </div>
@@ -72,17 +79,41 @@ const CartSingle = ({ data }) => {
     <div className="border-b p-4">
       <div className="w-full flex items-center">
         <div>
-        <div
+          <div
             className={`bg-[#e44343] border border-[#e4434373] rounded-full w-[25px] h-[25px] ${styles.noramlFlex} justify-center cursor-pointer`}
             onClick={() => setValue(value + 1)}
           >
             <HiPlus size={18} color="#fff" />
           </div>
+          <span className="pl-[10px]">{value}</span>
+          <div
+            className="bg-[#a7abb14f] rounded-full w-[25px] h-[25px] flex items-center justify-center cursor-pointer"
+            onClick={() => setValue(value === 1 ? 1 : value - 1)}
+          >
+            <HiOutlineMinus size={16} color="#7d879c" />
+          </div>
         </div>
+        <img
+          src="https://www.ivory.co.il/files/catalog/org/1683624597g97Su.jpg"
+          alt="product"
+          className="w-[130px] h-min ml-2 mr-2 rounded-[5px]"
+        />
+        <div className="pl-[5px]">
+          <h1>{data.name}</h1>
+          <h4 className="font-[400] text-[15px] text-[#00000082]">
+            ${data.price} * {value}
+          </h4>
+          <h4 className="font-[600] text-[17px] pt-[3px] text-[#d02222] font-Roboto">
+            US${totalPrice}
+          </h4>
+        </div>
+        <RxCross1
+          className="cursor-pointer"
+        />
       </div>
     </div>
-  )
-}
+  );
+};
 
 Cart.propTypes = {
   setOpenCart: PropTypes.func,

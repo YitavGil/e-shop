5e1810b30cd9986f8395b43cd8c12afe5fd9cd2d
YitavGil <yitavga@yahoo.com>
2023-06-13 08:53:16 +0300

    check authentication on login/signup pages

--- frontend/src/pages/LoginPage.jsx
+++ frontend/src/pages/LoginPage.jsx
@@ -1,7 +1,16 @@
-import React from 'react';
+import { useEffect } from 'react';
 import Login from "../components/login/Login.jsx";
+import { useNavigate } from 'react-router-dom';
+import { useSelector } from 'react-redux';
 
 const LoginPage = () => {
+  const navigate = useNavigate()
+  const { isAuthenticated } = useSelector((state) => state.user);
+  useEffect(() => {
+    if (isAuthenticated === true) {
+      navigate("/")
+    }
+  }, [])
   return (
     <div>
       <Login />
--- frontend/src/pages/SignupPage.jsx
+++ frontend/src/pages/SignupPage.jsx
@@ -1,6 +1,16 @@
-import Signup from '../components/signup/Signup'
+import { useEffect } from 'react';
+import Signup from '../components/signup/Signup';
+import { useNavigate } from 'react-router-dom';
+import { useSelector } from 'react-redux';
 
 const SignupPage = () => {
+  const navigate = useNavigate()
+  const { isAuthenticated } = useSelector((state) => state.user);
+  useEffect(() => {
+    if (isAuthenticated === true) {
+      navigate("/")
+    }
+  }, [])
   return (
     <div>
       <Signup />

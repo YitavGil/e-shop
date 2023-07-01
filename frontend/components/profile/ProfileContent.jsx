290bb63753068450083a117d956a3e68fc637bff
YitavGil <yitavga@yahoo.com>
2023-07-01 16:10:15 +0300

    profile content WIP

--- frontend/components/profile/ProfileContent.jsx
+++ frontend/components/profile/ProfileContent.jsx
@@ -6,10 +6,22 @@ import {
 } from "react-icons/ai";
 import { backend_url } from "../../src/server";
 import { useSelector } from "react-redux";
+import styles from "../../src/styles/styles";
 
 const ProfileContent = ({ active }) => {
   const { user } = useSelector((state) => state.user);
+  const [name, setName] = useState(user && user.name);
+  const [email, setEmail] = useState(user && user.email);
+  const [phoneNumber, setPhoneNumber] = useState(user && user.phoneNumber);
+  const [password, setPassword] = useState("");
+  const [zipCode, setZipCode] = useState(0);
+  const [avatar, setAvatar] = useState(null);
+
   const handleImage = () => {};
+
+  const handleSubmit = (e) => {
+    
+  };
 
   return (
     <div className="w-full">
@@ -31,13 +43,73 @@ const ProfileContent = ({ active }) => {
                   onChange={handleImage}
                 />
                 <label htmlFor="image">
-                  <AiOutlineCamera />
+                  <AiOutlineCamera className="cursor-pointer" />
                 </label>
               </div>
             </div>
           </div>
           <br />
           <br />
+          <div className="w-full px-5">
+          <form onSubmit={handleSubmit} aria-required={true}>
+              <div className="w-full sm:flex block pb-3">
+                <div className=" w-[100%] sm:w-[50%]">
+                  <label className="block pb-2">Full Name</label>
+                  <input
+                    type="text"
+                    className={`${styles.input} !w-[95%] mb-4 sm:mb-0`}
+                    required
+                    value={name}
+                    onChange={(e) => setName(e.target.value)}
+                  />
+                </div>
+                <div className=" w-[100%] sm:w-[50%]">
+                  <label className="block pb-2">Email Address</label>
+                  <input
+                    type="text"
+                    className={`${styles.input} !w-[95%] mb-1 sm:mb-0`}
+                    required
+                    value={email}
+                    onChange={(e) => setEmail(e.target.value)}
+                  />
+                </div>
+              </div>
+
+              <div className="w-full sm:flex block pb-3">
+                <div className=" w-[100%] sm:w-[50%]">
+                  <label className="block pb-2">Phone Number</label>
+                  <input
+                    type="number"
+                    className={`${styles.input} !w-[95%] mb-4 sm:mb-0`}
+                    required
+                    value={phoneNumber}
+                    onChange={(e) => setPhoneNumber(e.target.value)}
+                  />
+                </div>
+
+                <div className="w-[100%] sm:w-[50%]">
+                  <label className="block pb-2">Enter your password</label>
+                  <input
+                    type="password"
+                    className={`${styles.input} !w-[95%] mb-4 sm:mb-0`}
+                    required
+                    value={password}
+                    onChange={(e) => setPassword(e.target.value)}
+                  />
+                </div>
+              </div>
+              <input
+              style={{ 
+                color: '#3a24db',
+                border: '1px solid #3a24db'
+              }}
+                className={`w-[250px] p-2 h-[40px] text-center rounded-[3px] mt-8 cursor-pointer`}
+                required
+                value="Update"
+                type="submit"
+              />
+            </form>
+          </div>
         </>
       )}
     </div>

ccdf5b6983ab3712e439df93ff569b3c73b99006
Yitav <yitavga@yahoo.com>
2023-08-25 09:49:58 +0300

    shop create

--- backend/controller/shopController.js
+++ backend/controller/shopController.js
@@ -8,16 +8,67 @@ const { isAuthenticated, isSeller, isAdmin } = require("../middleware/auth");
 const cloudinary = require("cloudinary");
 const catchAsyncErrors = require("../middleware/catchAsyncErrors");
 const ErrorHandler = require("../utils/ErrorHandler");
-const sendShopToken = require("../utils/shopToken");
+// const sendShopToken = require("../utils/shopToken");
 const { upload } = require("../multer");
 
-router.post("/create-shop", upload.single("file"), async(req, res, next) => {
-    try {
-        const { email } = req.body;
-        const sellerEmail = await Shop.findOne({ email });
-    } catch (error) {
-        return next(new ErrorHandler(error.message, 400));
+router.post("/create-shop", upload.single("file"), async (req, res, next) => {
+  try {
+    const { email } = req.body;
+    const sellerEmail = await Shop.findOne({ email });
+    if (userEmail) {
+      const filename = req.file.filename;
+      const filePath = `uploads/${filename}`;
+      fs.unlink(filePath, (err) => {
+        if (err) {
+          console.log(err);
+          res.status(500).json({ message: "Error deleting file" });
+        }
+      });
+      return res
+        .status(400)
+        .json({ msg: "This email address is already being used." });
     }
-})
 
+    const filename = req.file.filename;
+    const fileUrl = path.join(filename);
+
+    const seller = {
+      name: req.body.name,
+      email: email,
+      password: req.body.password,
+      avatar: {
+        public_id: myCloud.public_id,
+        url: myCloud.secure_url,
+      },
+      address: req.body.address,
+      phoneNumber: req.body.phoneNumber,
+      zipCode: req.body.zipCode,
+    };
+
+    const activationToken = createActivationToken(seller);
+    const activationUrl = `http://127.0.0.1:5173/seller/activation/${activationToken}`;
+    try {
+      await sendMail({
+        email: seller.email,
+        subject: "Activate your Shop",
+        message: `Hello ${seller.name}, please click on the link to activate your shop: ${activationUrl}`,
+      });
+      res.status(201).json({
+        success: true,
+        message: `please check your email:- ${seller.email} to activate your shop!`,
+      });
+    } catch (error) {
+      return next(new ErrorHandler(error.message, 500));
+    }
+  } catch (error) {
+    return next(new ErrorHandler(error.message, 400));
+  }
+});
+
+// create activation token
+const createActivationToken = (user) => {
+  return jwt.sign(user, process.env.ACTIVATION_SECRET, {
+    expiresIn: "5m",
+  });
+};
 module.exports = router;

61db1cdb5bac93b3e296e802f9a26b5dc61efc84
Yitav <yitavga@yahoo.com>
2023-08-25 15:20:54 +0300

    shop controller wip

--- backend/controller/shopController.js
+++ backend/controller/shopController.js
@@ -71,4 +71,45 @@ const createActivationToken = (user) => {
     expiresIn: "5m",
   });
 };
+
+// activate user
+router.post(
+    "/activation",
+    catchAsyncErrors(async (req, res, next) => {
+      try {
+        const { activation_token } = req.body;
+  
+        const newSeller = jwt.verify(
+          activation_token,
+          process.env.ACTIVATION_SECRET
+        );
+  
+        if (!newSeller) {
+          return next(new ErrorHandler("Invalid token", 400));
+        }
+        const { name, email, password, avatar, zipCode, address, phoneNumber } =
+          newSeller;
+  
+        let seller = await Shop.findOne({ email });
+  
+        if (seller) {
+          return next(new ErrorHandler("User already exists", 400));
+        }
+  
+        seller = await Shop.create({
+          name,
+          email,
+          avatar,
+          password,
+          zipCode,
+          address,
+          phoneNumber,
+        });
+  
+        sendShopToken(seller, 201, res);
+      } catch (error) {
+        return next(new ErrorHandler(error.message, 500));
+      }
+    })
+  );
 module.exports = router;
